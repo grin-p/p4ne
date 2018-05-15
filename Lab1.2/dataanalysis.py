fd74bdf1bd63701d2f24247cd21aa3b3af179237
grin-p <grin-p@yandex.ru>
2018-05-15 19:08:28 +0300

    Lab 1.2

--- Lab1.2/dataanalysis.py
+++ Lab1.2/dataanalysis.py
@@ -11,6 +11,6 @@ Years_list = list(map(getvalue, sheet['A'][1:]))
 Temp_list = list(map(getvalue, sheet['C'][1:]))
 SunAct_list = list(map(getvalue, sheet['D'][1:]))
 
-pyplot.plot(Years_list, Temp_list, label="График температуры по годам")
-pyplot.plot(Years_list, SunAct_list, label="График температуры по годам")
+pyplot.plot(Years_list, Temp_list, label="Относит. температура")
+pyplot.plot(Years_list, SunAct_list, label="Активность солнца")
 pyplot.show()
