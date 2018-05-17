53bdb648f5ce83804bab62715b8bc4feb9375594
grin-p <grin-p@yandex.ru>
2018-05-17 19:05:18 +0300

    Lab 1.3

---
+++ Lab1.4/ipaddr.py
@@ -0,0 +1,33 @@
+from ipaddress import IPv4Network
+import random
+
+low_net=0x0B000000
+max_net=0xDF000000
+
+low_mask=8
+max_mask=24
+
+class IPv4RandomNetwork(IPv4Network):
+    def __init__(self, net_1=0x00000000, net_2=0xFFFFFFFF, mask_1=0, mask_2=32):
+        IPv4Network.__init__(self, (random.randint(net_1, net_2), random.randint(mask_1,mask_2)), strict=False)
+
+    def regular(self):
+        return self.is_global
+
+    def key_value(self):
+        key=int(self.network_address) + (int(self.hostmask) << 32)
+        return key
+
+def fun1(x):
+    return x.key_value()
+
+i=0
+L = []
+while i<50:
+    cur_net=IPv4RandomNetwork(low_net,max_net,low_mask,max_mask)
+    L.append(cur_net)
+#    print(str(cur_net))
+    i+=1
+
+for x in sorted(L, key=fun1):
+    print(x)
